626b517c14fdebb45a68bd15773f61f8491e2ed5
lov36 <000chucho@gmail.com>
2022-07-29 19:38:25 -0500

    tercera version

---
+++ ciclos.js
@@ -0,0 +1,30 @@
+// 1. Determinar los divisores de un número introducido por
+//teclado
+count = 0;
+while (count < 6) {
+    ++count
+    var num = 12;
+    var divs = num % count;
+
+    if (divs == 0) {
+        console.log(`${count} es divisor de ${num}`)
+    } else {
+        console.log(`${count} no es divisor de ${num}`)
+    }
+}
+
+
+//2. Determinar si un numero es o no es primo 
+pri = 1
+num = 2
+for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+        pri = 2
+    }
+}
+if (pri == 1) {
+    console.log(`${num} es primo`)
+} else if (pri == 2) {
+
+    console.log(`${num} no es primo`)
+}
